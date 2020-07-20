48a4423c48aecf446f0842db8f5a7c77fbdf20bd
ZehraRiz <zehrataqi@gmail.com>
2020-07-20 09:47:08 +0300

    added users with a username to another room

---
+++ backend/utils/users.js
@@ -0,0 +1,36 @@
+const users = [];
+
+//Join connection to player
+
+function userJoin(id, name, room="registeredUsers") {
+    const user = { id, name, room }
+    users.push(user);
+    return user;
+}
+
+//get current user
+
+function getCurrentUser(id) {
+    return users.find(user => user.id === id)
+}
+    //user leaves
+
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+//get room users
+function getRoomUsers() {
+    return users
+}
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+
+}
