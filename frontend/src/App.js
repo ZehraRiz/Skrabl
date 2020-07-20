d4386815a4dc6ae6a4c32373ec2a4eab5a1a768c
Thomas McAndrew <tommcandrew@hotmail.com>
2020-07-20 09:31:25 +0100

    add basic login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import io from "socket.io-client"
+import React from "react";
+import io from "socket.io-client";
+import Login from "./components/Login";
 
-function App() {
+const App = () => {
   const socket = io("http://localhost:4001");
-  
-  //When a new client connects 
-  socket.on('message',data=> {console.log(data)})
+
+  //When a new client connects
+  socket.on("message", (data) => {
+    console.log(data);
+  });
 
   //enter a username from the client'side. Hardcoding for now
-  socket.emit("username", "Zehra")
+  socket.emit("username", "Zehra");
 
   //invalid/empty username
-  socket.on("usernameError", data => { console.log(data) })
+  socket.on("usernameError", (data) => {
+    console.log(data);
+  });
 
   //on correct username, connection registered as a player list of all other players. need a way to keep this list updated. can be done at the front end too
-  socket.on("usernameRegistered", data=> {console.log(data)})
+  socket.on("usernameRegistered", (data) => {
+    console.log(data);
+  });
 
   //broadcast to other players that a new player has registered
-  socket.on("welcomeNewUser", data=> {console.log(data)})
+  socket.on("welcomeNewUser", (data) => {
+    console.log(data);
+  });
   return (
     <div>
-      Hello world
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
