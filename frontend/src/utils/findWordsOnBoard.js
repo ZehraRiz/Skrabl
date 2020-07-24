401c8f237db1f06bb5174d9880896bb1c0245cb5
John Darke <johndarke@live.co.uk>
2020-07-24 10:19:16 +0100

    add comments

--- frontend/src/utils/findWordsOnBoard.js
+++ frontend/src/utils/findWordsOnBoard.js
@@ -11,20 +11,20 @@ export const findWordsOnBoard = (boardState) => {
         for (var col = 0; col < 15; col++) { 
             var currentSquare = boardState[col + ((row * 15))];
             if (!currentSquare.hasOwnProperty('tile')) {  
-                if (wordStart !== ''){
+                if (wordStart !== ''){                          // tests if word is already being recorded
                     if (letters.length > 1) {
-                        words.push({word: letters.join(''), start: wordStart, dir: dir});
+                        words.push({word: letters.join(''), start: wordStart, dir: dir});     // captures word
                         console.log(words);
                     }
                 }
-                wordStart = '';
+                wordStart = '';       // ends recording of word
                 letters = [];
             } else {
                 letters.push(currentSquare.tile.letter);   
-                if (wordStart !== '' && col === 14 ) {
-                    words.push({word: letters.join(''), start: wordStart, dir: dir});
+                if (wordStart !== '' && col === 14 ) {                                  // includes last tile if word ends at end of board.
+                    words.push({word: letters.join(''), start: wordStart, dir: dir});           
                 } else if (wordStart == '') {
-                    wordStart = `${row}-${col}`;
+                    wordStart = `${row}-${col}`;                // begins recording word
                 }
             }
         }
@@ -35,19 +35,19 @@ export const findWordsOnBoard = (boardState) => {
         for (var row = 0; row < 15; row++) { 
             var currentSquare = boardState[col + ((row * 15))];
             if (!currentSquare.hasOwnProperty('tile')) { 
-                if (wordStart !== ''){
+                if (wordStart !== ''){                          // tests if word is already being recorded
                     if (letters.length > 1) {
-                        words.push({word: letters.join(''), start: wordStart, dir: dir});
+                        words.push({word: letters.join(''), start: wordStart, dir: dir});     // captures word
                     }
                 }
-                wordStart = '';
+                wordStart = '';     // ends recording of word
                 letters = [];
             } else {
                 letters.push(currentSquare.tile.letter);    
-                if (wordStart !== '' && row === 14 ) {
-                    words.push({word: letters.join(''), start: wordStart, dir: dir});
+                if (wordStart !== '' && row === 14 ) {                 
+                    words.push({word: letters.join(''), start: wordStart, dir: dir});   //  includes last tile if word ends at end of board.
                 } else if (wordStart == '') {
-                    wordStart = `${row}-${col}`;
+                    wordStart = `${row}-${col}`;                   // begins recording word
                 }
             }
         }
