3ab5c8235d18b0afde444e829f3f7e155f1f4956
John Darke <johndarke@live.co.uk>
2020-07-28 17:45:02 +0100

    modals fadeIn

--- frontend/src/components/NotificationModal.js
+++ frontend/src/components/NotificationModal.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../styles/NotificationModal.css";
+import { Fade } from "react-awesome-reveal";
 
 const NotificationModal = ({ notification, handleCloseNotificationModal }) => {
   return (
     <div className="notificationModal__wrapper">
-      <div className="notificationModal__content">
-        <p>{notification}</p>
-        <button onClick={handleCloseNotificationModal}>Close</button>
-      </div>
+      <Fade triggerOnce>
+        <div className="notificationModal__content">
+          <p>{notification}</p>
+          <button onClick={handleCloseNotificationModal}>Close</button>
+        </div>
+      </Fade>
     </div>
   );
 };
