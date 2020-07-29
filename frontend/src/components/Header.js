ae2e95a1b472c73d1c33401fd95edc595cb5d148
John Darke <johndarke@live.co.uk>
2020-07-29 11:10:13 +0100

    game rules and rulesModal added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/Header.css";
+import infoIcon from "../images/rules.svg";
 
-const Header = () => {
+
+const Header = ({handleClickRules}) => {
   return (
     <div className="header__wrapper">
       <h1>Skrabl</h1>
+      <div className="header__rules" onClick={handleClickRules} ><img src={infoIcon}/></div>
     </div>
   );
 };
