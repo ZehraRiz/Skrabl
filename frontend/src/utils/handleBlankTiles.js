bb3b98e561355281f81f0082587f0f7924ec4998
ZehraRiz <zehrataqi@gmail.com>
2020-08-04 17:44:19 +0300

    merged with master

--- frontend/src/utils/handleBlankTiles.js
+++ frontend/src/utils/handleBlankTiles.js
@@ -4,7 +4,6 @@ export const handleBlankTiles = (newWords, setConfirmMessage) => {
     newWords.map((newWord) => {
         newWord.squares.map(square => {
             if (square.tile.letter === "") {
-                console.log("encountered blank letter")
                 setConfirmMessage({
                     type: "blankTile",
                     message: "The word you made: "
