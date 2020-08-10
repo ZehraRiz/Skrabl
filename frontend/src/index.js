5d0cb286b73ee58e5eccaa52fa2c4f8b172bc958
John Darke <johndarke@live.co.uk>
2020-08-10 16:03:27 +0100

    div100

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Div100vh from 'react-div-100vh'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Div100vh>
-      <App />
-    </Div100vh>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
