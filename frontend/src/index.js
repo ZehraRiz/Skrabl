574b1bd81e335bc638835c4fc5f5b525f10454bc
John Darke <johndarke@live.co.uk>
2020-08-10 15:49:51 +0100

    100vh plugin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Div100vh from 'react-div-100vh'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Div100vh>
+      <App />
+    </Div100vh>
   </React.StrictMode>,
   document.getElementById("root")
 );
